78c5ba7152e9cc947d4ac896d08f881aaa08be39
Lucas Cordina <lucas.cordina92@gmail.com>
2018-04-05 21:40:20 +0200

    Initial Commit

---
+++ MarketMateTests/MarketMateTests.swift
@@ -0,0 +1,36 @@
+//
+//  MarketMateTests.swift
+//  MarketMateTests
+//
+//  Created by Lucas Cordina on 05/04/2018.
+//  Copyright © 2018 Lucas Cordina. All rights reserved.
+//
+
+import XCTest
+@testable import MarketMate
+
+class MarketMateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
